7847246ac9d7984b4c26f36bb4d55b3846510908
Shubham Kunal <shubhamkunal19@gmail.com>
2026-02-18 14:14:58 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -12,4 +12,8 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 module.exports = app;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,57 @@
-const User = require('../models/user.model');
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const createUser = async (data) => {
-    return await User.create(data);
+const dataPath = path.join(__dirname, '../data/users.json');
+
+const readData = async () => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+};
+
+const writeData = async (data) => {
+    await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+};
+
+const createUser = async (userData) => {
+    const users = await readData();
+    const newUser = { id: uuidv4(), ...userData, createdAt: new Date() };
+    users.push(newUser);
+    await writeData(users);
+    return newUser;
 };
 
 const getAllUsers = async () => {
-    return await User.find();
+    return await readData();
 };
 
 const getUserById = async (id) => {
-    return await User.findById(id);
+    const users = await readData();
+    return users.find(u => u.id === id);
 };
 
 const updateUser = async (id, data) => {
-    return await User.findByIdAndUpdate(id, data, { new: true });
+    const users = await readData();
+    const index = users.findIndex(u => u.id === id);
+    if (index === -1) return null;
+
+    users[index] = { ...users[index], ...data, updatedAt: new Date() };
+    await writeData(users);
+    return users[index];
 };
 
 const deleteUser = async (id) => {
-    return await User.findByIdAndDelete(id);
+    const users = await readData();
+    const index = users.findIndex(u => u.id === id);
+    if (index === -1) return null;
+
+    const deletedUser = users.splice(index, 1);
+    await writeData(users);
+    return deletedUser[0];
 };
 
 module.exports = {
